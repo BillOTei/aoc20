ee8f6bb08564b26a847e70cc0e45a398b32138b8
Alex <ateilhet@gmail.com>
2020-12-10 16:00:57 +0100

    day 10 part 2

--- 10/10.js
+++ 10/10.js
@@ -23,20 +23,39 @@ fs.readFile(process.argv[2], "utf8", function (err, contents) {
     }
   }
 
-  const run = (i, oneJoltCount, threeJoltsCount) => {
+  const nextAvailableAdapters = (joltage, incr, maxIncr, acc) => {
+    if (incr > maxIncr) {
+      return acc;
+    } else if (adaptersMap[joltage + incr]) {
+      return nextAvailableAdapters(joltage, incr + 1, maxIncr, [...acc, joltage + incr]);
+    } else {
+      return nextAvailableAdapters(joltage, incr + 1, maxIncr, acc);
+    }
+  }
+
+  const run1 = (i, oneJoltCount, threeJoltsCount) => {
     if (i === adaptersCount) {
       return oneJoltCount * threeJoltsCount;
     }
     const joltage = adapters[i];
     const nextAdapterJoltage = nextAvailableAdapter(joltage, 1, 3);
 
-    return run(
+    return run1(
       i + 1,
       nextAdapterJoltage - joltage === 1 ? oneJoltCount + 1 : oneJoltCount,
       nextAdapterJoltage - joltage === 3 ? threeJoltsCount + 1 : threeJoltsCount
     );
   }
 
+  let ways = adapters.map((x, i) => (i === 0 ? 1 : 0));
 
-  console.log(run(0, 0, 1));
+  for (let i = 0; i < ways.length; i++) {
+    for (let j = i - 3; j < i; j++) {
+      if (adapters[i] <= adapters[j] + 3) {
+        ways[i] += ways[j];
+      }
+    }
+  }
+
+  console.log(ways[adaptersCount - 1]);
 });
