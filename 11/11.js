deabd589741c9df73826750cf3a1ee43ac5b1a1c
Alex <ateilhet@gmail.com>
2020-12-11 18:17:09 +0100

    day 11 part 2

--- 11/11.js
+++ 11/11.js
@@ -39,8 +39,6 @@ fs.readFile(process.argv[2], "utf8", function (err, contents) {
       for (let j = 0; j < cols; j++) {
         prevGrid[i][j] = grid[i][j];
         grid[i][j] = nextGrid[i][j];
-
-        //nextGrid[i][j] = initState(i, j);
       }
     }
   }
@@ -52,10 +50,10 @@ fs.readFile(process.argv[2], "utf8", function (err, contents) {
     playing = true;
   }
 
-  function computeNextGen() {
+  function computeNextGen(emptyThreshold) {
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < cols; j++) {
-        applyRules(i, j);
+        applyRules(i, j, emptyThreshold);
       }
     }
 
@@ -67,9 +65,9 @@ fs.readFile(process.argv[2], "utf8", function (err, contents) {
 //   If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
 //   If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
 //   Otherwise, the seat's state does not change.
-  function applyRules(row, col) {
-    let numNeighbors = countNeighbors(row, col);
-    if (isAlive(row, col) && numNeighbors >= 4) {
+  function applyRules(row, col, emptyThreshold) {
+    let numNeighbors = countAllNeighbors(row, col);
+    if (isAlive(row, col) && numNeighbors >= emptyThreshold) {
       nextGrid[row][col] = 'L';
     } else if (grid[row][col] === 'L' && numNeighbors === 0) {
       nextGrid[row][col] = '#';
@@ -78,6 +76,10 @@ fs.readFile(process.argv[2], "utf8", function (err, contents) {
 
   function isAlive(row, col) {
     return grid[row][col] === '#';
+  }
+
+  function isFree(row, col) {
+    return grid[row][col] === 'L';
   }
 
   function countNeighbors(row, col) {
@@ -109,6 +111,102 @@ fs.readFile(process.argv[2], "utf8", function (err, contents) {
     return count;
   }
 
+  function countVertically(row, col, incr, count) {
+    if (incr < 0 && row + incr >= 0) {
+      if (isAlive(row + incr, col)) {
+        return count + 1;
+      } else if (isFree(row + incr, col)) {
+        return count;
+      } else {
+        return countVertically(row + incr, col, incr, count);
+      }
+    } else if (incr > 0 && row + incr < rows) {
+      if (isAlive(row + incr, col)) {
+        return count + 1;
+      } else if (isFree(row + incr, col)) {
+        return count;
+      } else {
+        return countVertically(row + incr, col, incr, count);
+      }
+    } else {
+      return count;
+    }
+  }
+
+  function countHorizontally(row, col, incr, count) {
+    if (incr < 0 && col + incr >= 0) {
+      if (isAlive(row, col + incr)) {
+        return count + 1;
+      } else if (isFree(row, col + incr)) {
+        return count;
+      } else {
+        return countHorizontally(row, col + incr, incr, count);
+      }
+    } else if (incr > 0 && col + incr < cols) {
+      if (isAlive(row, col + incr)) {
+        return count + 1;
+      } else if (isFree(row, col + incr)) {
+        return count;
+      } else {
+        return countHorizontally(row, col + incr, incr, count);
+      }
+    } else {
+      return count;
+    }
+  }
+
+  function countDiagonally(row, col, incrR, incrC, count) {
+    if (incrR < 0 && incrC < 0 && row + incrR >= 0 && col + incrC >= 0) {
+      if (isAlive(row + incrR, col + incrC)) {
+        return count + 1;
+      } else if (isFree(row + incrR, col + incrC)) {
+        return count;
+      } else {
+        return countDiagonally(row + incrR, col + incrC, incrR, incrC, count);
+      }
+    } else if (incrR < 0 && incrC > 0 && row + incrR >= 0 && col + incrC < cols) {
+      if (isAlive(row + incrR, col + incrC)) {
+        return count + 1;
+      } else if (isFree(row + incrR, col + incrC)) {
+        return count;
+      } else {
+        return countDiagonally(row + incrR, col + incrC, incrR, incrC, count);
+      }
+    } else if (incrR > 0 && incrC < 0 && row + incrR < rows && col + incrC >= 0) {
+      if (isAlive(row + incrR, col + incrC)) {
+        return count + 1;
+      } else if (isFree(row + incrR, col + incrC)) {
+        return count;
+      } else {
+        return countDiagonally(row + incrR, col + incrC, incrR, incrC, count);
+      }
+    } else if (incrR > 0 && incrC > 0 && row + incrR < rows && col + incrC < cols) {
+      if (isAlive(row + incrR, col + incrC)) {
+        return count + 1;
+      } else if (isFree(row + incrR, col + incrC)) {
+        return count;
+      } else {
+        return countDiagonally(row + incrR, col + incrC, incrR, incrC, count);
+      }
+    } else {
+      return count;
+    }
+  }
+
+  function countAllNeighbors(row, col) {
+    let count = 0;
+    count = countVertically(row, col, -1, count);
+    count = countVertically(row, col, 1, count);
+    count = countHorizontally(row, col, -1, count);
+    count = countHorizontally(row, col, 1, count);
+    count = countDiagonally(row, col, -1, -1, count);
+    count = countDiagonally(row, col, -1, 1, count);
+    count = countDiagonally(row, col, 1, -1, count);
+    count = countDiagonally(row, col, 1, 1, count);
+
+    return count;
+  }
+
   function isStable() {
     let check = true;
     outer:
@@ -127,7 +225,7 @@ fs.readFile(process.argv[2], "utf8", function (err, contents) {
 // Start everything
   initialize();
   while (playing) {
-    computeNextGen();
+    computeNextGen(5);
     if (isStable()) {
       playing = false;
     }
